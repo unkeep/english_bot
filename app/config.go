ec3e54f8497b3f28959be26fde51ab118854b414
Alexey Ankip <aliaksei.ankip@jobtome.com>
2022-08-03 13:58:37 +0400

    Fix cfg

--- app/config.go
+++ app/config.go
@@ -8,10 +8,6 @@ type config struct {
 	TgToken       string `required:"true"`
 	TgChatID      int64  `required:"true"`
 	TgAdminChatID int64  `required:"true"`
-	GSheetID      string `required:"true"`
-	GClientID     string `required:"true"`
-	GClientSecret string `required:"true"`
-	GProjectID    string `required:"true"`
 	MongoURI      string `required:"true"`
 }
 
